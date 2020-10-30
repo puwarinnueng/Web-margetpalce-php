638364933928517d51be4acfe7a247d53613e909
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2020-10-30 15:13:24 +0700

    detail  asus1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,27 +1,26 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Home extends CI_Controller {
+class Home extends CI_Controller
+{
 
 
-	public function index()
-	 {
-        
-        
-        
+    public function index()
+    {
+
+
+
         $this->load->view('navbar');
         $this->load->view('css');
         $this->load->view('home_view');
         $this->load->view('js');
-        
     }
 
     public function asus()
-	{
-        
+    {
+        $this->load->view('navbar');
+        $this->load->view('css');
         $this->load->view('asus');
+        $this->load->view('js');
     }
-
-
-
 }
--- application/views/asus.php
+++ application/views/asus.php
@@ -1,24 +1,28 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <style>
-    body {
-        background-color: powderblue;
-    }
+  body {
+    background-color: powderblue;
+  }
 </style>
 
 <head>
-	<meta charset="utf-8">
-	<title>com</title>
+  <meta charset="utf-8">
+  <title>LGA1200 Z490 Rog XI Formula</title>
 
 </head>
+
 <body style="text-align:center;">
-  
-  <img src="<?php echo base_url('img');?>/galax.jpg" alt=""
-  width="300px"
-  >
+
+  <!-- <img src="<?php echo base_url('img'); ?>/galax.jpg" alt="" width="300px"> -->
+  <img src="<?php echo base_url('img/menu1.jpg'); ?>" width="20%">
+
+  <br>
+  รายละเอียด
 
 
 </body>
