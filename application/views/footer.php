2eeb17d76cfc9cde14f7e5d662b89d79f248fd8b
Dullayut <theadfine@gmail.com>
2020-10-31 17:32:58 +0700

    footer #1

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,10 +1,35 @@
-<div class="container ft">
+  <div class="container-fluid ft">
     <div class="row">
-      <div class="col-xs-12 col-md-12">
-        <p align="center" class="ft">
-          @2020
-        </p>
+        <div class="col-sm-12 col-md-6">
+          <h6>blog แรก</h6>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>วิธีการชำระเงิน</h6>
+        </div>
+
+        <div class="col-xs-6 col-md-3">
+          <h6>ช่องการชำระเงิน</h6>
+        </div>
+
+        <!-- <div class="col-md-3">
+          <h6>บริการขนส่ง</h6>
+        </div> -->
+
       </div>
     </div>
   </div>
 
+  <style type="text/css">
+    .ft {
+      margin-top: 100px;
+      margin-bottom: 100px;
+      background-color: #2874f0;
+    }
+
+    .ft h6 {
+      color: #fff;
+      font-size: 16px;
+      margin-top: 5px;
+    }
+  </style>
