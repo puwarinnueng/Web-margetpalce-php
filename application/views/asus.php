b0c095f3f3dbf0f1d21f24f3121451304b828890
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2020-11-12 15:59:30 +0700

    1
    1

--- application/views/asus.php
+++ application/views/asus.php
@@ -17,12 +17,42 @@ defined('BASEPATH') or exit('No direct script access allowed');
 </head>
 
 <body style="text-align:center;">
-
-  <!-- <img src="<?php echo base_url('img'); ?>/galax.jpg" alt="" width="300px"> -->
-  <img src="<?php echo base_url('img/menu1.jpg'); ?>" width="20%">
-
   <br>
-  รายละเอียด
+  <img src="<?php echo base_url('img/menu1.jpg'); ?>" width="20%">
+  <br>
+  <h3>รายละเอียด</h3>
+  <br>
+  <table class="table">
+    <caption>ASUS</caption>
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+        <th>Username</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
 
 
 </body>
--- application/views/navbar.php
+++ application/views/navbar.php
@@ -8,10 +8,12 @@
       color: #FFFFFF;
 
     }
+
     .row1 {
       padding-top: 15px;
       padding-right: 50px;
     }
+
     /*test again*/
 
     .row2 {
@@ -266,7 +268,7 @@
 
         <div class="flipkart-navbar-search smallsearch col-sm-7 col-xs-11">
           <div class="row">
-            <input class="flipkart-navbar-input col-xs-11" type="" placeholder="Search for Products, Brands and more" name="">
+            <input class="flipkart-navbar-input col-xs-11" type="" placeholder="ค้นหาอุปกรณ์ที่คุณตองการ" name="">
             <button class="flipkart-navbar-button col-xs-1">
               <svg width="15px" height="15px">
                 <path d="M11.618 9.897l4.224 4.212c.092.09.1.23.02.312l-1.464 1.46c-.08.08-.222.072-.314-.02L9.868 11.66M6.486 10.9c-2.42 0-4.38-1.955-4.38-4.367 0-2.413 1.96-4.37 4.38-4.37s4.38 1.957 4.38 4.37c0 2.412-1.96 4.368-4.38 4.368m0-10.834C2.904.066 0 2.96 0 6.533 0 10.105 2.904 13 6.486 13s6.487-2.895 6.487-6.467c0-3.572-2.905-6.467-6.487-6.467 "></path>
@@ -299,9 +301,6 @@
             </div>
           </li>
         </ul>
-
-
-
       </div>
 
 
@@ -322,7 +321,7 @@
           <li class="upper-links"><a class="links" href="">mainboard</a></li>
           <li class="upper-links"><a class="links" href="">gpu</a></li>
           <li class="upper-links"><a class="links" href="">ram</a></li>
-          
+
 
           <li class="upper-links dropdown"><a class="links" href="http://clashhacks.in/">Dropdown</a>
             <ul class="dropdown-menu">
