50cc60a0a0d1200fce55698e784697828303811d
warin.noijun <warin.noijun@tesco.com>
2020-11-16 15:06:51 +0700

    Edit navbar

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -421,20 +421,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
       </div>
     </div>
-
-
-    <div class="container ft">
-      <div class="row">
-        <div class="col-xs-12 col-md-12">
-          <p align="center" class="ft">
-            @2020
-          </p>
-        </div>
-      </div>
-    </div>
-
-
-
   </body>
 
 </html>
--- application/views/js.php
+++ application/views/js.php
@@ -1,6 +1,6 @@
 <!-- js แบบ online -->
 <script type="text/javascript" src="
-<?php echo base_url('css/js.js');?>
+<?php echo base_url('css/js.js'); ?>
 "></script>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
@@ -10,8 +10,8 @@
 
 
 
-<!-- 
-<script>
+
+<!-- <script>
  function openNav() {
     document.getElementById("mySidenav").style.width = "70%";
     // document.getElementById("flipkart-navbar").style.width = "50%";
@@ -23,3 +23,4 @@ function closeNav() {
     document.body.style.backgroundColor = "rgba(0,0,0,0)";
 }
 </script> -->
+
