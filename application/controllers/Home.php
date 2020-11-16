ef700d6becfa8718423049658b28d2a73dae7ac9
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2020-11-16 23:30:25 +0700

    1

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -7,9 +7,6 @@ class Home extends CI_Controller
 
     public function index()
     {
-
-
-
         $this->load->view('navbar');
         $this->load->view('css');
         $this->load->view('home_view');
