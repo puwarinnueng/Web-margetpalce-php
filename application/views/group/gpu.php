c058c99be316a116f6c45be7e3bf2f152428f9fb
puwarinnueng <43135336+puwarinnueng@users.noreply.github.com>
2020-12-08 22:13:41 +0700

    1

--- application/views/group/gpu.php
+++ application/views/group/gpu.php
@@ -21,88 +21,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 <body style="text-align:center;">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- social -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <style>
-    body {
-      margin: 0;
-
-      height: 2000px;
-    }
-
-    .icon-bar {
-      position: fixed;
-      top: 76%;
-      -webkit-transform: translateY(-50%);
-      -ms-transform: translateY(-50%);
-      transform: translateY(-50%);
-    }
-
-    .icon-bar a {
-      display: block;
-      text-align: center;
-      padding: 16px;
-      transition: all 0.3s ease;
-      color: white;
-      font-size: 20px;
-    }
-
-    .icon-bar a:hover {
-      background-color: #000;
-    }
-
-    .facebook {
-      background: #3B5998;
-      color: white;
-    }
-
-    .twitter {
-      background: #55ACEE;
-      color: white;
-    }
-
-    .google {
-      background: #dd4b39;
-      color: white;
-    }
-
-    .linkedin {
-      background: #007bb5;
-      color: white;
-    }
-
-    .youtube {
-      background: #bb0000;
-      color: white;
-    }
-
-    .fa-instagram {
-      background: #125688;
-      color: white;
-    }
-
-    .content {
-      margin-left: 75px;
-      font-size: 30px;
-    }
-
-    .head-goods {
-      font-family: 'Prompt', sans-serif;
-      margin: 100px 0 10px;
-    }
-  </style>
-
-  <!-- social -->
-  <div class="icon-bar">
-    <a href="https://www.facebook.com/puwarin.nueng.92/" class="facebook"><i class="fa fa-facebook"></i></a>
-    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-    <a href="#" class="google"><i class="fa fa-google"></i></a>
-    <a href="https://www.youtube.com/watch?v=syFZfO_wfMQ" class="youtube"><i class="fa fa-youtube"></i></a>
-    <a href="#" class="fa fa-instagram"></a>
-  </div>
-  <!-- social -->
-
   <div class="head-goods">
     <h1 class="Best-sellers">GHARPHIC CARD</h1>
   </div>
@@ -117,7 +35,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <img src="<?php echo base_url('img/gpu1.png'); ?>">
           </div>
           <div class="contentBx">
-            <div class="brand"><a href="<?php echo site_url(''); ?>">ASUS RTX 2060 SUPER</a></div>
+            <div class="brand"><a href="<?php echo site_url(''); ?>">MSI RTX 2060 SUPER</a></div>
             <br><br>
             <h2 class="price">14,000.-</h2>
             <a class="buy" href="<?php echo site_url(''); ?>">เพิ่มใส่รถเข็น</a>
@@ -131,7 +49,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
             <img src="<?php echo base_url('img/gpu2.png'); ?>">
           </div>
           <div class="contentBx">
-            <div class="brand"><a href="<?php echo site_url(''); ?>">AORUS RTX 2070 SUPER</a></div>
+            <div class="brand"><a href="<?php echo site_url(''); ?>">MSI RTX 2070 SUPER</a></div>
             <br><br>
             <h2 class="price">19,000.-</h2>
             <a class="buy" href="<?php echo site_url(''); ?>">เพิ่มใส่รถเข็น</a>
@@ -142,12 +60,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/gpu3.png'); ?>">
           </div>
           <div class="contentBx">
-            <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
+            <div class="brand"><a href="<?php echo site_url(''); ?>">AORUS RTX 2080 SUPER</a></div>
             <br><br>
-            <h2 class="price">20000.-</h2>
+            <h2 class="price">25,000.-</h2>
             <a class="buy" href="<?php echo site_url(''); ?>">เพิ่มใส่รถเข็น</a>
           </div>
         </div>
@@ -156,12 +74,12 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/gpu4.png'); ?>">
           </div>
           <div class="contentBx">
-            <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
+            <div class="brand"><a href="<?php echo site_url(''); ?>">GALAX RTX 2080 TI</a></div>
             <br><br>
-            <h2 class="price">20000.-</h2>
+            <h2 class="price">45,000.-</h2>
             <a class="buy" href="<?php echo site_url(''); ?>">เพิ่มใส่รถเข็น</a>
           </div>
         </div>
--- application/views/home_view.php
+++ application/views/home_view.php
@@ -165,89 +165,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
   </script>
 
 
-  <!-- social -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <style>
-    body {
-      margin: 0;
-
-      height: 2000px;
-    }
-
-    .icon-bar {
-      position: fixed;
-      top: 76%;
-      -webkit-transform: translateY(-50%);
-      -ms-transform: translateY(-50%);
-      transform: translateY(-50%);
-    }
-
-    .icon-bar a {
-      display: block;
-      text-align: center;
-      padding: 16px;
-      transition: all 0.3s ease;
-      color: white;
-      font-size: 20px;
-    }
-
-    .icon-bar a:hover {
-      background-color: #000;
-    }
-
-    .facebook {
-      background: #3B5998;
-      color: white;
-    }
-
-    .twitter {
-      background: #55ACEE;
-      color: white;
-    }
-
-    .google {
-      background: #dd4b39;
-      color: white;
-    }
-
-    .linkedin {
-      background: #007bb5;
-      color: white;
-    }
-
-    .youtube {
-      background: #bb0000;
-      color: white;
-    }
-
-    .fa-instagram {
-      background: #125688;
-      color: white;
-    }
-
-    .content {
-      margin-left: 75px;
-      font-size: 30px;
-    }
-
-    .head-goods {
-      font-family: 'Prompt', sans-serif;
-      margin: 100px 0 10px;
-    }
-  </style>
-
- 
-  <div class="icon-bar">
-
-    <a href="https://www.facebook.com/puwarin.nueng.92/" class="facebook"><i class="fa fa-facebook"></i></a>
-    <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
-    <a href="#" class="google"><i class="fa fa-google"></i></a>
-    <a href="https://www.youtube.com/watch?v=syFZfO_wfMQ" class="youtube"><i class="fa fa-youtube"></i></a>
-    <a href="#" class="fa fa-instagram"></a>
-  </div>
-  <!-- social -->
-
   <div class="head-goods">
     <h1 class="Best-sellers">โปรโมชั่น</h1>
   </div>
@@ -259,7 +176,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -273,7 +190,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -287,7 +204,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -301,7 +218,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -323,7 +240,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -337,7 +254,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -351,7 +268,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -365,7 +282,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -387,7 +304,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -401,7 +318,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -415,7 +332,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
@@ -429,7 +346,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
       <div class="col-md-3">
         <div class="card-product">
           <div class="imgBx">
-            <img src="<?php echo base_url('img/1.png'); ?>">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="contentBx">
             <div class="brand"><a href="<?php echo site_url(''); ?>">LGA1200 Z490 Rog XI Formula</a></div>
