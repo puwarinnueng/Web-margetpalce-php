6985bf77a42a5db6dd0453bf96e312fe031ff7af
warin.noijun <warin.noijun@tesco.com>
2020-12-09 00:53:56 +0700

    Add product_detail

--- application/views/product_detail/css_product_detail.php
+++ application/views/product_detail/css_product_detail.php
@@ -1,25 +1,102 @@
-
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
 
 <style>
-    *{
+    * {
         margin: 0;
         padding: 0;
     }
 
-    html{
+    html {
         margin: 0;
         padding: 0;
     }
 
-    body{
+    body {
         margin: 0;
         padding: 0;
         height: 100hv;
-        display: flex;
         align-items: center;
+        align-content: center;
+        font-family: 'Prompt', sans-serif;
+
+    }
+
+    .card_product_detail {
+        background: #f6f6f6;
+        height: 100%;
+        width: 100%;
+        padding: 20px;
+        border: 4px solid #DEDDDD;
+        display: flex;
+
+    }
+
+    .card_product_detail .img_detail,
+    .card_product_detail .infos {
+        width: 50%;
+    }
+
+
+    .infos {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        overflow: clip;
+
+    }
+
+    .infos p {
+        text-align: left;
+        font-size: 12px;
+    }
+
+    .name_detail {
+        font-size: 24px;
+        font-weight: bold;
+        text-align: left;
+    }
+
+    .price_detail {
+        font-size: 21px;
+        font-weight: bold;
+        text-align: left;
+        color: red;
+    }
+
+    .guarantee {
+        text-align: left;
+    }
+
+    .buttons_detail {
+        height: 50px;
+        width: 150px;
+        align-items: center;
+        align-content: center;
+        font-size: 16px;
+        font-weight: bold;
+        border: none;
+        background: #ff9d47;
+        border-radius: 10px;
+    }
+
+    .buttons_detail a {
+        color: #003559;
+    }
+
+    .buttons_detail a:hover {
+        color: white;
+        text-decoration: none;
+    }
+
+    .img_detail img {
+        width: 85%;
+        height: 60%;
+        margin-top: 22%;
+        margin-right: 10%;
     }
 </style>
--- application/views/product_detail/mb_asus.php
+++ application/views/product_detail/mb_asus.php
@@ -8,41 +8,34 @@ defined('BASEPATH') or exit('No direct script access allowed');
   <meta charset="utf-8">
   <title>LGA1200 Z490 Rog XI Formula</title>
 </head>
-<br><br>
+<br><br><br>
+
 <body>
   <div class="container">
     <div class="row">
       <div class="col-md-8">
         <div class="card_product_detail">
           <div class="img_detail">
+            <img src="<?php echo base_url('img/mb1.png'); ?>">
           </div>
           <div class="infos">
-            <h1>MAINBOARD (เมนบอร์ด) 1200 ASUS ROG MAXIMUS XII FORMULA </h1>
-          </div>
-          <div class="more-info">
+            <h1 class="name_detail">MAINBOARD (เมนบอร์ด) 1200 ASUS ROG MAXIMUS XII FORMULA </h1><br>
             <p>- แบรนด์ ASUS </p>
             <p>- LGA 1200 socket : รองรับหน่วยประมวลผล Intel® Core™ 10th-gen</p>
-            <p>- Robust power delivery:ระบบการจ่ายไฟแบบ 16 power stages, พร้อมจุดเชื่อมต่อแบบ ProCool II , MicroFine Alloy chokes และ 10K capacitors</p>
-            <p>- Cooling & PerformanceOptiMem III, CrossChill EK III, 16 teamed power stages ด้วย extended heatsink, high conductive thermal pad, ROG water cooling zone, ฮีทซิงค์ M.2 และ backplate</p>
-            <p>- Connectivity: Intel® Wi-Fi 6, 10 Gb Ethernet and Intel® 2.5 Gb Ethernet, triple M.2 slots</p>
-            <p>- Customization :2" LiveDash OLED, 2X addressable header + 2X 4-pin RGB header</p>
+            <p>- SOCKET : 1200</p>
+            <p>- CHIPSET : INTEL Z490</p>
+            <p>- MEMORY : 4 X DDR4 DIMM</p>
+            <br>
+            <h4 class="guarantee"><i class="fa fa-certificate" aria-hidden="true" style="font-size:20px;color:#FACC2E"></i> ประกัน 3 ปี</h4><br><br>
+            <h3 class="price_detail"> ราคา 18,400 บาท</h3>
+            <br>
+            <p class="availbilty">AVAILABILITY : In Stock</p>
+            <br>
+            <button class="buttons_detail"><a href="<?php echo site_url(''); ?>"><i class="fa fa-shopping-cart" aria-hidden="true"></i> เพิ่มใส่รถเข็น </button>
           </div>
-          <div class="price_detail">
-              <h3> ราคา 18,400 บาท</h3>
-          </div>
-
-          <div class="guarantee"></div>
-            <h4><i class="fa fa-certificate" aria-hidden="true" style="font-size:20px;color:#FACC2E"></i> ประกัน 3 ปี</h4>
-        </div>
-        <div class="availbilty">
-            <p>AVAILABILITY : In Stock</p>
-        </div>
-        <div class="buttons_detail">
-          <button><a href="<?php echo site_url(''); ?>"><i class="fa fa-shopping-cart" aria-hidden="true"></i> เพิ่มใส่รถเข็น</button>
         </div>
       </div>
     </div>
-
   </div>
 
 
