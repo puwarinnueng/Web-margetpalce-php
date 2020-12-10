4c8714beb647f7681343714f87d14547dc230502
Dullayut <theadfine@gmail.com>
2020-12-10 14:54:33 +0700

    create 3 mainboard pages

--- application/views/product_detail/mb_aorus.php
+++ application/views/product_detail/mb_aorus.php
@@ -6,13 +6,103 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 <head>
   <meta charset="utf-8">
-  <title>LGA1200 Z490 Rog XI Formula</title>
+  <title>LGA1200 GIGABYTE Z490 AORUS XTREME</title>
 </head>
 
 <body>
+  <div class="container">
+    <div class="row row1">
+      <div class="col-md-8">
+        <div1 class="card_product_detail">
+          <div class="img_detail">
+            <img src="<?php echo base_url('img/mb3.png'); ?>">
+          </div>
+          <div class="infos">
+            <h1 class="name_detail">MAINBOARD (เมนบอร์ด) 1200 GIGABYTE Z490 AORUS XTREME </h1><br>
+            <p>- แบรนด์ GIGABYTE </p>
+            <p>- LGA 1200 socket : รองรับหน่วยประมวลผล Intel® Core™ 10th-gen</p>
+            <p>- SOCKET : 1200</p>
+            <p>- CHIPSET : INTEL Z490</p>
+            <p>- MEMORY : 4 X DDR4 DIMM</p>
+            <br>
+            <h4 class="guarantee"><i class="fa fa-certificate" aria-hidden="true" style="font-size:20px;color:#FACC2E"></i> ประกัน 3 ปี</h4><br><br>
+            <h3 class="price_detail"> ราคา 24,900 บาท</h3>
+            <br>
+            <p class="availbilty">AVAILABILITY : In Stock</p>
+            <br>
+            <button class="buttons_detail"><a href="<?php echo site_url(''); ?>"><i class="fa fa-shopping-cart" aria-hidden="true"></i> เพิ่มใส่รถเข็น </a></button>
+          </div>
+      </div>
 
+      <div class="col-md-4">
+        <div class="card_right">
+          <table class="card_right_table">
+            <tr>
+              <td><i class="fas fa-thumbs-up" id="thumbs_up_icon1"></i> รับประกันสินค้าแท้ 100%</td>
+            </tr>
+            <tr>
+              <td><i class="fas fa-shipping-fast" id="truck_icon"></i> ส่งฟรีทั่วไทย
+                <p style="font-size: 10px; ">ช้อปครบ 3000.- ขึ้นไป</p>
+              </td>
+            </tr>
+            <tr>
+              <td><i class="fa fa-exchange" aria-hidden="true" id="exchange_icon"></i> สามารถเปลี่ยนสินค้าได้
+                <p style="font-size: 10px;"> ภายใน 7 วัน</p>
+              </td>
+            </tr>
+            <tr>
+              <td class="pay"><i class="fas fa-credit-card" id="creditcard_icon1"></i> ชำระเงินได้หลายช่องทาง
+                <p style="font-size: 10px;">- เก็บเงินปลายทาง</p>
+                <p style="font-size: 10px;">- โอนผ่านทางธนาคาร/ATM</p>
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
 
+    </div>
+  </div>
+
+  <div class="row row2">
+    <div class="col-md-8">
+      <div class="discription">
+        <h3>รายละเอียดสินค้า</h3>
+        <div class="content_discription">
+          <table class="discription_table">
+            <tr>
+              <td>Brand </td>
+              <td>GIGABYTE</td>
+            </tr>
+            <tr>
+              <td>Model </td>
+              <td>Z490 AORUS XTREME</td>
+            </tr>
+            <tr>
+              <td>CPU </td>
+              <td>Support for 10th Generation Intel® Core™ i9 processors/Intel® Core™ i7 processors/ Intel® Core™ i5 processors/Intel® Core™ i3 processors/Intel® Pentium® processors/ Intel® Celeron® processors in the LGA1200 package</td>
+            </tr>
+            <tr>
+              <td>CPU Series </td>
+              <td>Intel Celeron , Intel Core i3 , Intel Core i5 , Intel Core i7 , Intel Core i9 , Intel Pentium Gold</td>
+            </tr>
+            <tr>
+              <td>Chipset </td>
+              <td>Z490</td>
+            </tr>
+            <tr>
+              <td>จำนวน Slot แรม </td>
+              <td>4</td>
+            </tr>
+            <tr>
+              <td>ความจุแรมสูงสุด </td>
+              <td>128 GB</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
 </body>
-
 
 </html>
