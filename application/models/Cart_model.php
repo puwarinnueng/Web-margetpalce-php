d744d4ca4ef0ee86ebf8f9e4d6d2e62660d1505c
Dullayut <theadfine@gmail.com>
2020-12-15 02:07:21 +0700

    commit

--- application/models/Cart_model.php
+++ application/models/Cart_model.php
@@ -355,4 +355,15 @@ class Cart_model extends CI_Model
         $query = $this->db->get();
         return $query->result();
     }
+
+    public function searching($searchs)
+    {
+        $this->db->select('*');
+        $this->db->from('tbl_cart');
+        $this->db->like('p_name', $searchs);
+        $this->db->or_like('p_type', $searchs);
+        $query = $this->db->get();
+        return $query->result();
+    }
+
 }
