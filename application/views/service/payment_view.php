20125a1f672b5f57594fb5c87f8448964fbc8ce4
Dullayut <theadfine@gmail.com>
2020-12-16 21:55:40 +0700

    update order_payment_about_search

--- application/views/service/payment_view.php
+++ application/views/service/payment_view.php
@@ -27,7 +27,38 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
     <div class="container">
     <div class="how_to_pay">
-        
+        <br>
+        <h2>หลังจากยืนยันการสั่งซื้อสินค้าแล้ว ก็เลือกระหว่างชำระเงินปลาย หรือชำระผ่านบัญชีธนาคาร</h2>
+        <br>
+        <img src="<?php echo base_url('img/buying_step8.jpg'); ?>">
+        <br><br><br>
+        <h3><a href="<?php echo site_url('home/how_order'); ?>">ดูวิธีการสั่งซื้อสินค้าออนไลน์</a></h3><br><br>
+
+        <div class="pay_by_bank">
+            <h1 class="step_head"><b>กรณีชำระผ่านบัญชีธนาคาร</b></h1>
+            <br>
+            <h2>เมื่อเลือกชำระผ่านบัญชีธนาคาร ก็จะให้เลือกธนาคารตามไอคอนโลโก้
+                <br> และกดปุ่ม "ตกลงการสั่งซื้อ"
+            </h2>
+            <br>
+            <img src="<?php echo base_url('img/payment_bank1.jpg'); ?>">
+            <br><br><br><br>
+
+            <h2>จากนั้นจะขึ้นหน้าสรุปคำสั่งซื้อทั้งหมด ให้ [1] แนบสลิปโอนเงินให้เรียบร้อย <br>จากนั้นจึงกดปุ่ม [2] "อัปโหลดสลิป"</h2>
+            <br>
+            <img src="<?php echo base_url('img/payment_bank1.jpg'); ?>">
+            <br><br><br><br>
+        </div>
+
+        <div class="pay_on_delivery">
+            <h1 class="step_head"><b>กรณีชำระเงินปลายทาง</b></h1>
+            <br>
+            <h2>หากเลือกชำระเงินปลายทาง จะถูกนำมาที่หน้าสรุปคำสั่งซื้อเลย <br>โดยให้กดปุ่ม "อัปโหลดสลิป" <b>โดยที่ไม่ต้องแนบสลิปมาด้วย</b></h2>
+            <br>
+            <img src="<?php echo base_url('img/payment_bank1.jpg'); ?>">
+            <br><br><br><br>
+        </div>
+
     </div>
     </div>
 
